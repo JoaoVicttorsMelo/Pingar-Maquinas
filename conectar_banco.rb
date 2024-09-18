78b1670851aa8e15ce7956de8aea6e232da97879
joaovicttorsmelo <joaovicttorsilveiramelo@gmail.com>
2024-09-18 17:38:01 -0300

    Projeto quase finalizado, protegendo informações de smpt e criando modulo de enviar e-mail para possiveis futuras aplicações

--- conectar_banco.rb
+++ conectar_banco.rb
@@ -1,62 +1,101 @@
 require 'sqlite3'
+require_relative 'enviar_email'
 
 class ConectarBanco
-  def initialize(caminho_db = "C:/Users/joao.silveira/Desktop/Projetos_Programas/projeto_etl/Verificar_DashBoard/dist/Banco_Dados/comparacao.db")
+  include EnviarEmail
+  def initialize(caminho_db)
     @banco = caminho_db
-    conexao
+    abrir_conexao
   end
-
-  def conexao
-    begin
-    @db= SQLite3::Database.new @banco
-    rescue SQLite3::Exception => e
-      puts e.message
-    end
+  def abrir_conexao
+    @db = SQLite3::Database.new(@banco)
+    puts "Conexão estabelecida com sucesso"
+  rescue SQLite3::Exception => e
+    puts "Erro ao estabelecer conexão: #{e.message}"
   end
-
   def fechar_conexao
-    begin
-      @db.clone if @db
-      puts "Fechando conexão"
-    end
-  end
-
-  def script (script)
-    if comando_permitido(script)
-      begin
-        rows = @db.execute script
-        i=0
-        rows.each do |row|
-          puts "#{i+=1} - #{row.join(" ")}"
-        end
-      rescue SQLite3::Exception => e
-        puts e.message
-      end
+    if @db
+      @db.close
+      puts "Conexão fechada com sucesso"
     else
-      puts "Usuario não é capaz de fazer qualquer modificação, apenas consulta"
+      puts "Nenhuma conexão ativa para fechar"
     end
+  rescue SQLite3::Exception => e
+    puts "Erro ao fechar a conexão: #{e.message}"
   end
-
-  def ping (script)
-    if comando_permitido(script)
-      begin
-        rows = @db.execute script
-        rows.each do |row|
-           system("ping #{row.join(" ")}")
-        end
-      rescue SQLite3::Exception => e
-        puts e.message
+  def script(script)
+    executar_comando(script) do |rows|
+      rows.each_with_index do |row, i|
+        puts "#{i + 1} - #{row.join(' ')}"
       end
-    else
-      puts "Usuario não é capaz de fazer qualquer modificação, apenas consulta"
+    end
+  end
+  def ping(script)
+    ips_falhos = []
+    executar_comando(script) do |rows|
+      rows.each do |row|
+        ip = row.join(' ')
+        puts "Pingando IP: #{ip}"
+
+        falhas = 0
+        3.times do
+          if system("ping -n 1 #{ip}")
+            puts "#{ip} respondeu ao ping"
+          else
+            puts "#{ip} não respondeu ao ping"
+            falhas += 1
+          end
+        end
+
+        # Se falhar 2 ou mais vezes, adicionar à lista de IPs falhos
+        if falhas >= 2
+          puts "#{ip} falhou em 2 ou mais tentativas. Adicionando à lista de falhas."
+          ips_falhos << ip
+        else
+          puts "#{ip} respondeu com sucesso ao menos 2 vezes."
+        end
+      end
+    end
+
+    # Mostrar os IPs que falharam
+    if ips_falhos.any?
+      # Formatar IPs para o SQL corretamente
+      ip_list = ips_falhos.map { |ip| "'#{ip}'" }.join(', ')
+      lojas_com_erro = []
+
+      # Executar comando com WHERE IN para múltiplos IPs
+      executar_comando("SELECT FILIAL, COD_FILIAL, CNPJ FROM filiais_ip WHERE IP IN (#{ip_list})") do |linhas|
+        linhas.each do |linha|
+          filial = linha[0]
+          cod_filial = linha[1]
+          cnpj = linha[2]
+          info_formatada = "Filial - #{filial} - (#{cod_filial.to_s.rjust(6,'0')}) do CNPJ: #{cnpj}, favor verificar"
+
+          # Adicionar a informação formatada ao array
+          lojas_com_erro << info_formatada
+        end
+      end
+
+      # Enviar o e-mail se houver lojas com erro
+      if lojas_com_erro.any?
+        receber_email = ["joao.silveira@viaveneto.com.br","ti.loja@viaveneto.com.br"]
+        enviar_email("Lojas sem conexão com a Matriz", "FILIAIS COM ERRO", lojas_com_erro.join("<br>"),receber_email)
+      end
     end
   end
 
-  def comando_permitido(script)
-    begin
-      script.strip.downcase.start_with?('select')
+  def executar_comando(script)
+    if comando_permitido?(script)
+      rows = @db.execute(script)
+      yield(rows) if block_given?
+    else
+      puts "Usuário não é capaz de fazer qualquer modificação, apenas consulta"
     end
+  rescue SQLite3::Exception => e
+    puts "Erro ao executar o comando: #{e.message}"
+  end
+  def comando_permitido?(script)
+    script.strip.downcase.start_with?('select')
   end
 end
-
 
--- verificar_maquinas.rb
+++ verificar_maquinas.rb
@@ -1,5 +1,12 @@
 require_relative 'conectar_banco'
-obj = ConectarBanco.new
-obj.script("SELECT ip FROM FILIAIS_IP where servidor=1")
-#obj.ping("SELECT ip FROM FILIAIS_IP where servidor=1")
+require 'yaml'
+
+config_path = File.join(__dir__, 'config.yml')
+config = YAML.load_file(config_path)
+
+caminho_db = config['database']['db']
+
+obj = ConectarBanco.new(caminho_db)
+#obj.script("SELECT ip FROM FILIAIS_IP where servidor=1")
+obj.ping("SELECT ip FROM FILIAIS_IP where servidor=1 ORDER BY COD_FILIAL")
 obj.fechar_conexao
